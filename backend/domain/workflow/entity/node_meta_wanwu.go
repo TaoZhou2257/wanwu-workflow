2499d83dd7e226ad3954420ffa7acc88709634c6
王亮 <wangl1158@chinaunicom.cn>
2025-08-26 15:58:07 +0800

    feat(wanwu): 增加知识库、文档解析、文档生成节点

--- backend/domain/workflow/entity/node_meta_wanwu.go
+++ backend/domain/workflow/entity/node_meta_wanwu.go
@@ -2,11 +2,68 @@ package entity
 
 // Wanwu Node Type Definition
 const (
-	NodeTypeWanwuIntentDetector NodeType = "WanwuIntentDetector"
+	NodeTypeWanwuIntentDetector     NodeType = "WanwuIntentDetector"
+	NodeTypeWanWuKnowledgeRetriever NodeType = "WanWuKnowledgeRetriever"
+	NodeTypeWanWuFileGenerator      NodeType = "WanWuFileGenerator"
+	NodeTypeWanWuFileParser         NodeType = "WanWuFileParser"
 )
 
 // Wanwu NodeTypeMetas Init
 func init() {
+	NodeTypeMetas[NodeTypeWanWuKnowledgeRetriever] = &NodeTypeMeta{
+		ID:           1006,
+		Key:          NodeTypeWanWuKnowledgeRetriever,
+		DisplayKey:   "Dataset",
+		Name:         "知识库检索",
+		Category:     "data",
+		Desc:         "在选定的知识中,根据输入变量召回最匹配的信息,并以列表形式返回",
+		Color:        "#FF811A",
+		IconURL:      "https://lf3-static.bytednsdoc.com/obj/eden-cn/dvsmryvd_avi_dvsm/ljhwZthlaukjlkulzlp/icon/icon-KnowledgeQuery-v2.jpg",
+		SupportBatch: false,
+		ExecutableMeta: ExecutableMeta{
+			PreFillZero: true,
+			PostFillNil: true,
+		},
+		EnUSName:        "Knowledge retrieval",
+		EnUSDescription: "In the selected knowledge, the best matching information is recalled based on the input variable and returned as an Array.",
+	}
+
+	NodeTypeMetas[NodeTypeWanWuFileGenerator] = &NodeTypeMeta{
+		ID:           1007,
+		Key:          NodeTypeWanWuFileGenerator,
+		DisplayKey:   "FileGenerator",
+		Name:         "文档生成",
+		Category:     "data",
+		Desc:         "基于生成的文档标题和文档内容生成文档",
+		Color:        "#FF811A",
+		IconURL:      "https://lf3-static.bytednsdoc.com/obj/eden-cn/dvsmryvd_avi_dvsm/ljhwZthlaukjlkulzlp/icon/icon-KnowledgeQuery-v2.jpg",
+		SupportBatch: false,
+		ExecutableMeta: ExecutableMeta{
+			PreFillZero: true,
+			PostFillNil: true,
+		},
+		EnUSName:        "File generator",
+		EnUSDescription: "Generate documents using both the document title and content.",
+	}
+
+	NodeTypeMetas[NodeTypeWanWuFileParser] = &NodeTypeMeta{
+		ID:           1008,
+		Key:          NodeTypeWanWuFileParser,
+		DisplayKey:   "FileParser",
+		Name:         "文档解析",
+		Category:     "data",
+		Desc:         "用于解析传入文档内容",
+		Color:        "#FF811A",
+		IconURL:      "https://lf3-static.bytednsdoc.com/obj/eden-cn/dvsmryvd_avi_dvsm/ljhwZthlaukjlkulzlp/icon/icon-KnowledgeQuery-v2.jpg",
+		SupportBatch: false,
+		ExecutableMeta: ExecutableMeta{
+			PreFillZero: true,
+			PostFillNil: true,
+		},
+		EnUSName:        "File generator",
+		EnUSDescription: "Parse documents content.",
+	}
+
 	NodeTypeMetas[NodeTypeWanwuIntentDetector] = &NodeTypeMeta{
 		ID:           1022,
 		Key:          NodeTypeWanwuIntentDetector,
@@ -25,4 +82,5 @@ func init() {
 		EnUSName:        "Intent recognition",
 		EnUSDescription: "Used for recognizing the intent in user input and matching it with preset intent options.",
 	}
+
 }
