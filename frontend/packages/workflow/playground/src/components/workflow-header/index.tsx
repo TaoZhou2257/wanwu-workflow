f648851236c4ac565dbce03c87d242c5e1e388cd
dysiliky <dysiliky@163.com>
2025-12-19 11:16:47 +0800

    chore(wanwu): the workflow for publishing does not display the publishing history

--- frontend/packages/workflow/playground/src/components/workflow-header/index.tsx
+++ frontend/packages/workflow/playground/src/components/workflow-header/index.tsx
@@ -38,6 +38,7 @@ const WorkFlowHeader: React.FC = () => {
   const globalState = useGlobalState();
   const { readonly, info, playgroundProps, workflowId } = globalState;
 
+  const isPublished = info.plugin_id !== '0';
   return (
     <div className={styles.container}>
       <div
@@ -71,7 +72,8 @@ const WorkFlowHeader: React.FC = () => {
             <SubmitButton />
           </>
         )}
-        <HistoryButton />
+
+        {isPublished && <HistoryButton />}
 
         <PublishButton />
 
